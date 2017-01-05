7b86cdf40ce853e4f10befbc31887a0ee6c4ba47
0x00-0x00 <andre.marques@fatec.sp.gov.br>
2017-01-05 11:37:27 -0200

    Added GPG-key addition support

--- owncloud_client-ubuntu.sh
+++ owncloud_client-ubuntu.sh
@@ -9,6 +9,18 @@ if [ "$uid" != "0" ]; then
 fi
 
 
+# Variables definition 
+UBUNTU_1610=http://download.opensuse.org/repositories/isv:/ownCloud:/desktop/Ubuntu_16.10/
+UBUNTU_1610_KEY=http://download.opensuse.org/repositories/isv:ownCloud:desktop/Ubuntu_16.10/Release.key
+
+UBUNTU_16=http://download.opensuse.org/repositories/isv:/ownCloud:/desktop/Ubuntu_16.04/
+UBUNTU_16_KEY=http://download.opensuse.org/repositories/isv:ownCloud:desktop/Ubuntu_16.04/Release.key
+
+UBUNTU_15=http://download.opensuse.org/repositories/isv:/ownCloud:/desktop/Ubuntu_15.04/
+UBUNTU_15_KEY=http://download.opensuse.org/repositories/isv:ownCloud:desktop/Ubuntu_15.04/Release.key
+
+
+
 # For ubuntu 15.10
 # sh -c "echo 'deb http://download.opensuse.org/repositories/isv:/ownCloud:/desktop/Ubuntu_15.10/ /' > /etc/apt/sources.list.d/owncloud-client.list"
 
@@ -19,14 +31,23 @@ fi
 # sh -c "echo 'deb http://download.opensuse.org/repositories/isv:/ownCloud:/desktop/Ubuntu_15.04/ /' > /etc/apt/sources.list.d/owncloud-client.list"
 
 #For ubuntu 16.04
+echo "[+] Downloading GPG key of OwnCloud repository ..."
+wget ${UBUNTU_16_KEY} -O /tmp/Release.key > /dev/null 2>&1
+
+echo "[+] Adding key to apt-get ... "
+apt-key add - < /tmp/Release.key
+
+echo "[*] Cleaning unneccessary files ..."
+shred -u -z /tmp/Release.key
+
 echo "[+] Adding owncloud desktop repository to sources.list ... "
-sh -c "echo 'deb http://download.opensuse.org/repositories/isv:/ownCloud:/desktop/Ubuntu_16.04/ /' > /etc/apt/sources.list.d/owncloud-client.list"
+sh -c "echo 'deb ${UBUNTU_16} /' > /etc/apt/sources.list.d/owncloud-client.list"
 
 echo "[+] Updating package headers ... "
 apt-get update > /dev/null 2>&1
 
 echo "[+] Installing owncloud-client ..."
-apt-get install owncloud-client > /dev/null 2>&1
+apt-get install owncloud-client -y > /dev/null 2>&1
 
 # Complete
 echo "[*] OwnCloud-client package has been installed."
