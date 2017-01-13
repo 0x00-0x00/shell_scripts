788cb755464e900cca7eb61a9c742ffcb627db20
0x00-0x00 <andre.marques@fatec.sp.gov.br>
2017-01-13 02:46:46 -0200

    Rewritten script, version 2.0

--- ssh_publicAuth.sh
+++ ssh_publicAuth.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
 
-echo "Type remote host user: "
-read USER
-
-echo "Type remote host IP: " 
-read IP
-
-if [ -a "~/.ssh/id_rsa.pub" ]; then
-	echo "[*] Local public key was found."
+function public_key
+{
+user_name=$(id -u -n)
+if [ -e "/home/$user_name/.ssh/id_rsa.pub" ]; then
+	echo "[+] Local public key was found."
 else
-	echo "[!] Local public key was not found."
-	ssh-keygen
-	
+	echo "[-] Local public key was not found."
+	ssh-keygen	
+fi
+}
+
+
+x=0
+for arg in "$@"
+do
+((x++));
+done
+
+# Check argument number
+if [ "$x" != "2" ]; then
+	echo "Usage: $0 USER SERVER_IP"
+	exit
 fi
 
-cat ~/.ssh/id_rsa.pub | ssh ${USER}@${IP} 'cat >> ~/.ssh/authorized_keys'
+# Check public key and/or generate it.
+public_key
 
-echo "Procedure complete."
+# Copy content of public key into authorized_keys file of server
+cat ~/.ssh/id_rsa.pub | ssh $1@$2 'cat >> ~/.ssh/authorized_keys'
+
+echo "[+] Procedure complete."
 exit
