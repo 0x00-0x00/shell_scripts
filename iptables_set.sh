80368d96992ce6fddc79858f1c7e2a2f12ee6552
0x00-0x00 <andre.marques@fatec.sp.gov.br>
2017-01-13 02:00:45 -0200

    VERSION 2.0

--- iptables_set.sh
+++ iptables_set.sh
@@ -1,84 +1,80 @@
 #!/bin/bash
 
-#Check if user is root as changing firewall settings are privileged actions.
+# ------------------------ #
+# Script escrito por zc00l #
+# ------------------------ #
+
+# Static variables 
+SERVER_IP=($(hostname -I))
+ALLOW_PORTS=(22 25 53 80 443 3128 5222)
+IPT=$(which iptables)
+
+function clean_iptables 
+{
+	echo "[+] Clearing existing rules ..."
+	${IPT} -F;
+}
+
+function allow_port 
+{
+	${IPT} -A INPUT -p tcp -d "$2" --sport $1 -m state --state ESTABLISHED -j ACCEPT;
+	echo "[+] Created new rule: ACCEPT for $1 in CHAIN INPUT."
+	${IPT} -A OUTPUT -p tcp -s "$2" --dport $1 -m state --state NEW,ESTABLISHED -j ACCEPT;
+	echo "[+] Created new rule: ACCEPT for $1 in CHAIN OUTPUT."
+}
+
+function allow_dns
+{
+	${IPT} -A INPUT -p udp --dport 53 -m state --state NEW,ESTABLISHED -j ACCEPT;
+	${IPT} -A OUTPUT -p udp --sport 53 -m state --state ESTABLISHED -j ACCEPT;
+	${IPT} -A OUTPUT -p udp --dport 53 -m state --state NEW,ESTABLISHED -j ACCEPT;
+	${IPT} -A INPUT -p udp --sport 53 -m state --state ESTABLISHED -j ACCEPT;
+	echo "[+] Created ruleset for DNS queries for IP $1."
+}
+
+function check_root 
+{
+	if [ "$1" != "0" ]; then
+		echo "[!] You lack privileges to run this script."
+		exit
+	fi
+}
+
+function enable_log
+{
+	${IPT} -A INPUT -j LOG -m limit --limit 12/min --log-level 4 --log-prefix 'INPUT drop:'
+	echo "[+] Logging enabled for chain INPUT."
+	${IPT} -A INPUT -j DROP
+	echo "[+] Chain INPUT set to DROP."
+	${IPT} -A OUTPUT -j LOG -m limit --limit 12/min --log-level 4 --log-prefix 'OUTPUT drop: '
+	echo "[+] Logging enabled for chain OUTPUT."
+	${IPT} -A OUTPUT -j DROP
+	echo "[+] Chain OUTPUT set to DROP."
+}
+
+# Script init
+echo "[*] Checking privileges ..."
 uid=$(id -u)
+check_root $uid
+clean_iptables
 
-if [ "$uid" != "0" ]; then
-	echo "You must have super-user privileges to run this script."
-	exit
-fi
-
-#------------------------
-# Script configuration #
-#------------------------
-server_ip=192.168.0.123
-allow_ports=(22 25 53 80 443 5222)
-#outgoing_only=(10000:65535)
-outgoing_only=(10000:10001)
-
-# Zeroing the configuration first
-iptables -F
-
-# Allowing DNS queries
-echo "[+] Setting up DNS rules ..."
-iptables -A INPUT -p udp -d "${server_ip}" --dport 53 -m state --state ESTABLISHED -j ACCEPT;
-iptables -A INPUT -p udp -d "${server_ip}" --sport 53 -m state --state ESTABLISHED -j ACCEPT;
-
-iptables -A INPUT -p tcp -d "${server_ip}" --dport 53 -m state --state ESTABLISHED -j ACCEPT;
-iptables -A INPUT -p tcp -d "${server_ip}" --sport 53 -m state --state ESTABLISHED -j ACCEPT;
-
-iptables -A OUTPUT -p udp -s "${server_ip}" --sport 53 -m state --state NEW,ESTABLISHED -j ACCEPT;
-
-# Not needed.
-iptables -A OUTPUT -p udp -s "${server_ip}" --dport 53 -m state --state NEW,ESTABLISHED -j ACCEPT;
-#iptables -A OUTPUT -p tcp -d "${server_ip}" --dport 53 -m state --state NEW,ESTABLISHED -j ACCEPT;
-#iptables -A OUTPUT -p tcp -s "${server_ip}" --sport 53 -m state --state NEW,ESTABLISHED -j ACCEPT;
-
-
-# Loop configuration
-# --------------------
-# Iterator variable
-x=0
-
-#Number of ports to be allowed (n)
-y=6
-
-while [ "$x" != "$y" ]; 
+for host in "${SERVER_IP[@]}"
 do
-	echo "[+] Creating rules for port ${allow_ports[$x]} ..."; 
-	#iptables -A INPUT -p tcp -d "${server_ip}" --sport ${allow_ports[$x]} -m state --state ESTABLISHED -j ACCEPT;
-	iptables -A INPUT -p tcp -d "${server_ip}" --dport ${allow_ports[$x]} -m state --state NEW,ESTABLISHED -j ACCEPT;
-	#iptables -A OUTPUT -p tcp -d "${server_ip}" --dport ${allow_ports[$x]} -m state --state NEW,ESTABLISHED -j ACCEPT;
-	iptables -A OUTPUT -s "${server_ip}" -p tcp --sport ${allow_ports[$x]} -m state --state NEW,ESTABLISHED -j ACCEPT;
-	((x++)); 
+	echo "[+] Creating ruleset for IP ${host} ..."
+	allow_dns $host
+	# Loop array into function
+	for port in "${ALLOW_PORTS[@]}"
+	do
+		allow_port $port $host
+	done
 done
-	echo ""
-	echo "[*] Incoming/Outgoing rules ....... OK"
-	echo ""
 
-# Loop configuration
-# --------------------
-#Iterator variable
-x=0
-#Number of ports to create rules
-y=1
+echo "[+] ${#ALLOW_PORTS[*]} ports were set to permissive rules in iptables."
 
-while [ "$x" != "$y" ];
-do
-	echo "[+] Creating outgoing rules for port/ports ${outgoing_only[$x]} ...";
-	#iptables -A OUTPUT -p tcp --dport ${outgoing_only[$x]} -j ACCEPT;
-	iptables -A OUTPUT -p tcp -s "${server_ip}" --sport ${outgoing_only[$x]} -m state --state NEW,ESTABLISHED -j ACCEPT;
-	((x++));
-done	
-	echo ""
-	echo "[*] Outgoing-only rules ........... OK"
-	echo ""
+enable_log
 
-echo "[*] Changing default policy to DROP and enabling logs..."
-iptables -A INPUT -j LOG -m limit --limit 12/min --log-level 4 --log-prefix 'IP INPUT drop: '
-iptables -A INPUT -j DROP
+echo "[X] End of script."
+exit
 
-iptables -A OUTPUT -j LOG -m limit --limit 12/min --log-level 4 --log-prefix 'IP OUTPUT drop: '
-iptables -A OUTPUT -j DROP
 
-echo "[+] All done."
+
