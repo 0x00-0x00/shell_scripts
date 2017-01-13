087898859dfec42be39bd234cafcdf95e3a8fccc
0x00-0x00 <andre.marques@fatec.sp.gov.br>
2017-01-12 23:02:30 -0200

    New iptables script

--- iptables_set.sh
+++ iptables_set.sh
@@ -8,34 +8,56 @@ if [ "$uid" != "0" ]; then
 	exit
 fi
 
+#------------------------
+# Script configuration #
+#------------------------
+server_ip=192.168.0.123
+allow_ports=(22 25 53 80 443 5222)
+#outgoing_only=(10000:65535)
+outgoing_only=(10000:10001)
 
-allow_ports=(22 25 80 443 5222)
-outgoing_only=(10000:65535)
+# Zeroing the configuration first
+iptables -F
 
-###################################
-#       LOOP CONFIGURATION        #
-###################################
-#Iterator variable
+# Allowing DNS queries
+echo "[+] Setting up DNS rules ..."
+iptables -A INPUT -p udp -d "${server_ip}" --dport 53 -m state --state ESTABLISHED -j ACCEPT;
+iptables -A INPUT -p udp -d "${server_ip}" --sport 53 -m state --state ESTABLISHED -j ACCEPT;
+
+iptables -A INPUT -p tcp -d "${server_ip}" --dport 53 -m state --state ESTABLISHED -j ACCEPT;
+iptables -A INPUT -p tcp -d "${server_ip}" --sport 53 -m state --state ESTABLISHED -j ACCEPT;
+
+iptables -A OUTPUT -p udp -s "${server_ip}" --sport 53 -m state --state NEW,ESTABLISHED -j ACCEPT;
+
+# Not needed.
+iptables -A OUTPUT -p udp -s "${server_ip}" --dport 53 -m state --state NEW,ESTABLISHED -j ACCEPT;
+#iptables -A OUTPUT -p tcp -d "${server_ip}" --dport 53 -m state --state NEW,ESTABLISHED -j ACCEPT;
+#iptables -A OUTPUT -p tcp -s "${server_ip}" --sport 53 -m state --state NEW,ESTABLISHED -j ACCEPT;
+
+
+# Loop configuration
+# --------------------
+# Iterator variable
 x=0
 
 #Number of ports to be allowed (n)
-y=5
+y=6
+
 while [ "$x" != "$y" ]; 
 do
 	echo "[+] Creating rules for port ${allow_ports[$x]} ..."; 
-	iptables -A INPUT -p tcp --dport ${allow_ports[$x]} -j ACCEPT;
-	iptables -A INPUT -p tcp --sport ${allow_ports[$x]} -j ACCEPT;
-	iptables -A OUTPUT -p tcp --sport ${allow_ports[$x]} -j ACCEPT;
-	iptables -A OUTPUT -p tcp --dport ${allow_ports[$x]} -j ACCEPT;
+	#iptables -A INPUT -p tcp -d "${server_ip}" --sport ${allow_ports[$x]} -m state --state ESTABLISHED -j ACCEPT;
+	iptables -A INPUT -p tcp -d "${server_ip}" --dport ${allow_ports[$x]} -m state --state NEW,ESTABLISHED -j ACCEPT;
+	#iptables -A OUTPUT -p tcp -d "${server_ip}" --dport ${allow_ports[$x]} -m state --state NEW,ESTABLISHED -j ACCEPT;
+	iptables -A OUTPUT -s "${server_ip}" -p tcp --sport ${allow_ports[$x]} -m state --state NEW,ESTABLISHED -j ACCEPT;
 	((x++)); 
 done
 	echo ""
 	echo "[*] Incoming/Outgoing rules ....... OK"
 	echo ""
 
-###################################
-#       LOOP CONFIGURATION        #
-###################################
+# Loop configuration
+# --------------------
 #Iterator variable
 x=0
 #Number of ports to create rules
@@ -43,17 +65,20 @@ y=1
 
 while [ "$x" != "$y" ];
 do
-	echo "[+] Creating outgoing rules ...";
-	iptables -A OUTPUT -p tcp --dport ${outgoing_only[$x]} -j ACCEPT;
-	iptables -A OUTPUT -p tcp --sport ${outgoing_only[$x]} -j ACCEPT;
+	echo "[+] Creating outgoing rules for port/ports ${outgoing_only[$x]} ...";
+	#iptables -A OUTPUT -p tcp --dport ${outgoing_only[$x]} -j ACCEPT;
+	iptables -A OUTPUT -p tcp -s "${server_ip}" --sport ${outgoing_only[$x]} -m state --state NEW,ESTABLISHED -j ACCEPT;
 	((x++));
 done	
 	echo ""
 	echo "[*] Outgoing-only rules ........... OK"
 	echo ""
 
-echo "[*] Changing default policy to DROP ..."
-iptables -P INPUT DROP
-iptables -P OUTPUT DROP
+echo "[*] Changing default policy to DROP and enabling logs..."
+iptables -A INPUT -j LOG -m limit --limit 12/min --log-level 4 --log-prefix 'IP INPUT drop: '
+iptables -A INPUT -j DROP
+
+iptables -A OUTPUT -j LOG -m limit --limit 12/min --log-level 4 --log-prefix 'IP OUTPUT drop: '
+iptables -A OUTPUT -j DROP
 
 echo "[+] All done."
