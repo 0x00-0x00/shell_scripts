c7b6243a1fd9699fb1a568a2f484878f15c9c158
shemhazai <andre.marques@fatec.sp.gov.br>
2017-02-15 16:16:12 -0200

    Host-only adapter script for VirtualBox

--- vbox_adapter.sh
+++ vbox_adapter.sh
@@ -17,8 +17,8 @@ function check_binaries
     binaries=("modprobe" "vboxmanage")
     for binary in "${binaries[@]}"
     do
-        echo "${binary}: "
-        which ${binary}
+        echo -n "${binary}: "
+        which ${binary} > /dev/null 2>&1
         if [[ $? != 0 ]]; then
             echo "FAIL"
         else
@@ -44,8 +44,25 @@ function probe_modules
     echo ""
 }
 
+function get_interface_name
+{
+    # Passes the virtual box interface name to a variable given to this function
+    data=$(ifconfig vboxnet | grep mtu);
+    iface_name=$(echo $data | grep -oP '.+(?=:)');
+    eval="$1='$iface_name'";
+    return 0;
+}
+
 function create_adapter
 {
+    echo "[+] Enabling virtual interface to host system ..."
+    echo -n "Virtual host-only adapter: ";
+    if_n=$(ifconfig vboxnet | grep mtu | wc -l)
+    if [[ $if_n -gt 0 ]]; then
+        echo "OK"
+        return 1;
+    fi
+
     echo -n "Creating host-only adapter: "
     vboxmanage hostonlyif create > /dev/null 2>&1
     if [[ $? == 0 ]]; then
@@ -53,6 +70,21 @@ function create_adapter
     else
         echo "FAIL";
     fi
+}
+
+function configure_adapter
+{
+    # Get the interface name
+    iface_name=''
+    get_interface_name iface_name
+    echo -n "Configuring interface $iface_name: "
+    vboxmanage hostonlyif ipconfig $iface_name --ip 192.168.56.1 --netmask 255.255.255.0
+    if [[ $? != 0 ]]; then
+        echo "FAIL";
+    else
+        echo "OK";
+    fi
+    return 0
 }
 
 function enable_dhcp_server
@@ -69,4 +101,4 @@ function enable_dhcp_server
 check_binaries
 probe_modules
 create_adapter
-enable_dhcp_server
+configure_adapter
