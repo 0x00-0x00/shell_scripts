4f4cc7bfa7c0789cc65f8457a97490a6e6a58c69
z3r0-c00l <andre.marques@fatec.sp.gov.br>
2017-02-22 11:17:19 -0300

    Several fixes and improvements

--- squid-config.sh
+++ squid-config.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 uid=$(id -u)
+conf_template=conf/squid/squid.conf
 conf_file=/etc/squid3/squid.conf
 back_file=/etc/squid3/squid.conf.old
 
@@ -59,21 +60,73 @@ function backup_conf
 }
 
 
+function customize_config
+{
+    #  Check template exists.
+    if [[ ! -e $conf_template ]]; then
+        echo "[!] Configuration file template does not exists.";
+        exit;
+    fi
+
+    local placeholder=XXX
+    lines=()
+    data=$(cat $conf_template | grep "$placeholder")
+    while  read -r line;
+    do
+        lines+=("$line");
+    done <<< "$data";
+
+    #  Create temporary file
+    tmp_file=$(mktemp)
+    echo "[*] Using temporary file $tmp_file"
+
+    #  Dump template data into temporary
+    cat $conf_template > $tmp_file;
+
+    echo "[*] Interactive configuration started."
+    for l in "${lines[@]}";
+    do
+        parameter=$(echo $l | awk {'print $1'})
+        description="$(cat $tmp_file | grep -B1 "$parameter" | head -n1)"
+        echo $description;
+
+
+        echo -n "[$parameter] value: "
+        read tmp_value;
+
+        f_line=$(echo "$tmp_value" | sed "s/\//\\\\\//g")
+        replace_string="$(cat "$tmp_file" | grep "$parameter" | sed "s/$placeholder/$f_line/")"
+
+        f_line=$(echo "$replace_string" | sed "s/\//\\\\\//g")
+
+        fo_line=$(echo "$l" | sed "s/\//\\\\\//g")
+        repl_data=$(sed "s/$fo_line/$f_line/" $tmp_file);
+
+        echo "$repl_data" > $tmp_file;
+        echo "[+] Parameter '$parameter' successfully set to '$tmp_value'.";
+        echo "";
+        sleep 1;
+    done
+
+    cat $tmp_file;
+    return 0;
+}
+
 #  Check for necessary privileges
-check_root
+#check_root
 
 #  Check for squid installation
-check_squid
+#check_squid
 
 
 #  If not installed, install it.
-if [[ $? != 0 ]]; then
-    install_squid;
-fi
+#if [[ $? != 0 ]]; then
+#    install_squid;
+#fi
 
 
 # Backup the configuration file
-backup_conf
+#backup_conf
 
-
+customize_config
 
