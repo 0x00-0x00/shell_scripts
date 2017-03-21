4b372115892e4e42cd377403436d0f019e2ae0b5
Z3R0C00L <andre.marques@fatec.sp.gov.br>
2017-03-20 23:12:39 -0300

    Adding a script to change /etc/resolv.conf

---
+++ change_resolv.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+function log
+{
+    echo -e "\033[092mINFO\033[0m: $1";
+    return 0;
+}
+
+function error
+{
+    echo -e "\033[091mERROR\033[0m: $1";
+    return 0;
+}
+
+
+uid=$(id -u);
+if [[ $uid != 0 ]]; then
+    error "Apenas o root pode alterar o arquivo /etc/resolv.conf!";
+    exit 0;
+fi
+
+if [[ $1 == "" ]]; then
+    error "Sem argumentos o suficiente.";
+    log "Uso: $0 -s SERVER_DNS";
+    exit 0;
+fi
+
+while getops "s:" opt; do
+    case $opt in
+        s)SERVER_DNS=$OPTARG; log "Servidor DNS definido em: $OPTARG";;
+    esac
+done
+
+sed "s/[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/${SERVER_DNS}/g" /etc/resolv.conf
+if [[ $? == 0 ]]; then
+    log "DNS alterado com sucesso!";
+else
+    error "Nao foi possivel alterar o DNS.";
+    exit 1;
+fi
+
+exit 0;
