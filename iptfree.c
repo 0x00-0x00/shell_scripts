b57c457573c72cc91326c360e00c468fcec6fda6
Z3R0C00L <andre.marques@fatec.sp.gov.br>
2017-05-01 13:38:48 -0300

    program to free all iptables rules and policies

---
+++ iptfree.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdlib.h>
+
+
+int main(int argc, char* argv[])
+{
+    int user_id, return_code;
+    user_id = geteuid();
+    if (user_id != 0) {
+        fprintf(stderr, "Not enough privileges to run this program.\n");
+        exit(1);
+    }
+
+    return_code = system("iptables -P INPUT ACCEPT; iptables -P OUTPUT ACCEPT; iptables -P FORWARD; iptables -F");
+    if ( return_code != 0 ) {
+        fprintf(stdout, "ERROR: Could not free the iptables rules.\n");
+        exit(1);
+    }
+    fprintf(stdout, "IPTables ruleset was successfully cleansed.\n");
+    return 0;
+}
